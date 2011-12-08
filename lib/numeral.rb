2d1c9ff13e964bf00ae5bba2251592fe0d1548a7
Michael (Doc) Norton <docondev@gmail.com>
2011-12-07 20:56:42 -0500

    ditching roman numeral

--- lib/numeral.rb
+++ lib/numeral.rb
@@ -1,56 +1,21 @@
 class RomanNumeral
-    SYMBOLS = { 1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 9 => "IX", 5 => "V", 1 => "I" }
-
-    def initialize
-        @roman_numeral = ""
-        @prior_digit = 10000
-        @prior_numeral = ""
-    end
-
+    NUMERALS = { 1 => "I", 5 => "V", 10 => "X" }
     def convert(number)
-        SYMBOLS.each { |digit, numeral|
-            while number >= digit
-                number = find_symbol_match(number, digit, numeral)
-                number = build_subtractions(number, digit, numeral)
-                number = build_additions(number, digit, numeral)
-            end
-
-            @prior_digit = digit
-            @prior_numeral = numeral
-        }
-        @roman_numeral
+        result = process_additions(number)
+        result = process_symbols(number) if not result
+        result
     end
-
-    private
-
-    def find_symbol_match(number, digit, numeral)
-        if number == digit
-            @roman_numeral += numeral
-            number = 0
+    def process_symbols(number)
+        NUMERALS[number]
+    end
+    def process_additions(number)
+        result = ""
+        while number > 0
+            result += "I"
+            number -= 1
         end
-        return number
-    end
-
-    def build_subtractions(number, digit, numeral)
-        if number_between_symbols(number, digit)
-            @roman_numeral += numeral + @prior_numeral 
-            number -= (@prior_digit - digit)
-        end
-        return number
-    end
-
-    def build_additions(number, digit, numeral)
-        while number >= digit
-            @roman_numeral += numeral
-            number -= digit
-        end
-        number
-    end
-
-    def number_between_symbols(number, digit)
-        range_top = @prior_digit-digit
-        range_bottom = @prior_digit
-
-        return number.between?(range_top, range_bottom)
+        return result
+        return "III" if number == 3
+        return "II" if number == 2
     end
 end
--- spec/numeral_spec.rb
+++ spec/numeral_spec.rb
@@ -1,43 +1,26 @@
 require "numeral"
 
-describe "@roman.Numerals" do
+describe "Roman Numerals" do
     before(:each) do
-        @roman = RomanNumeral.new
+        @numeral = RomanNumeral.new
     end
     context "Base Numbers" do
         it "should convert number 1 to numeral I" do
-            @roman.convert(1).should == "I"
+            @numeral.convert(1).should == "I"
         end
-
         it "should convert number 5 to numeral V" do
-            @roman.convert(5).should == "V"
+            @numeral.convert(5).should == "V"
         end
-
         it "should convert number 10 to numeral X" do
-            @roman.convert(10).should == "X"
-        end
-
-    end
-    context "Additions" do
-        it "should convert number 3 to III" do
-            @roman.convert(3).should == "III"
+            @numeral.convert(10).should == "X"
         end
     end
-    context "Subtractions" do
-        it "should convert number 4 to IV" do
-            @roman.convert(4).should == "IV"
+    context "Manage Additions" do
+        it "should convert number 3 to numeral III" do
+            @numeral.convert(3).should == "III"
         end
-
-        it "should convert number 9 to IX" do
-            @roman.convert(9).should == "IX"
-        end
-
-        it "should convert number 49 to XLIX" do
-            @roman.convert(49).should == "XLIX"
-        end
-
-        it "should convert number 40 to XL" do
-            @roman.convert(40).should == "XL"
+        it "should convert number 2 to numeral II" do
+            @numeral.convert(2).should == "II"
         end
     end
 end
