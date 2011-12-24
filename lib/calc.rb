7ce7bedb70fc9652f469c2c91ce97987dad1ce54
Michael (Doc) Norton <docondev@gmail.com>
2011-12-24 13:07:41 -0500

    Sum strings of 0,1, or 2 digits

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,5 +1,8 @@
 class Calc
-    def add(input)
-        0
+    def add(numbers)
+        total = 0
+        nums = numbers.split(',')
+        nums.each{ |num| total += num.to_i }
+        total
     end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,10 +1,20 @@
 require "calc"
 
 describe "String Calculator" do
+    let(:calc) {Calc.new}
+
     describe "add" do
         it "should return 0 for an empty string" do
-            calc = Calc.new
             calc.add("").should == 0
+        end
+        it "should return 1 for a string of '1'" do
+            calc.add("1").should == 1
+        end
+        it "should return 20 for a string of '20'" do
+            calc.add("20").should == 20
+        end
+        it "should add numbers delimited with a comma" do
+            calc.add("1,2").should == 3
         end
     end
 end
