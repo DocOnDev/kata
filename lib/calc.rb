12566ae89a31fa8ee5ea36fa39c12eb5c30b4bd3
Michael (Doc) Norton <docondev@gmail.com>
2011-12-24 21:30:31 -0500

    Custom delimiters and negative numbers

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,8 +1,50 @@
 class Calc
+    attr_accessor :numbers
+
     def add(numbers)
+        @numbers = numbers
+        return total(@numbers.split(build_delimiter))
+    end
+
+    private
+
+    def total(nums)
+        return negative_warning(nums) if contains_negatives?(nums)
         total = 0
-        nums = numbers.split(',')
         nums.each{ |num| total += num.to_i }
         total
+    end
+
+    def build_delimiter
+        delimiter = %r{\n|,}
+        if has_custom_delimiter
+            delimiter = build_custom_delimiter
+        end
+        delimiter
+    end
+
+    def has_custom_delimiter
+        @numbers.slice(0..1) == "//"
+    end
+
+    def build_custom_delimiter
+        @numbers.slice!(0..1)
+        parts = @numbers.split("\n",2)
+        @numbers = parts[1]
+        parts[0]
+    end
+
+    def contains_negatives?(nums)
+        gather_negatives(nums).length > 0
+    end
+
+    def negative_warning(nums)
+        message = "negatives not allowed:"
+        gather_negatives(nums).each{ |neg| message << " " << neg }
+        message
+    end
+
+    def gather_negatives(nums)
+        nums.select{ |num| num.to_i < 0 }
     end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -4,17 +4,50 @@ describe "String Calculator" do
     let(:calc) {Calc.new}
 
     describe "add" do
-        it "should return 0 for an empty string" do
-            calc.add("").should == 0
+        describe "simple strings" do
+            it "should return 0 for an empty string" do
+                calc.add("").should == 0
+            end
+            it "should return 1 for a string of '1'" do
+                calc.add("1").should == 1
+            end
+            it "should return 20 for a string of '20'" do
+                calc.add("20").should == 20
+            end
         end
-        it "should return 1 for a string of '1'" do
-            calc.add("1").should == 1
+        describe "comma delimited" do
+            it "should return 3 for a string of '1,2'" do
+                calc.add("1,2").should == 3
+            end
+            it "should return 20 for a string of '1,2,3,4,10'" do
+                calc.add("1,2,3,4,10").should == 20
+            end
         end
-        it "should return 20 for a string of '20'" do
-            calc.add("20").should == 20
+        describe "newline delimited" do
+            it "should support a newline delimiter" do
+                calc.add("1\n2\n3").should == 6
+            end
         end
-        it "should add numbers delimited with a comma" do
-            calc.add("1,2").should == 3
+        describe "newline and comma delimited" do
+            it "should return 4 for a string of '1,1\n2'" do
+                calc.add("1,1\n2").should == 4
+            end
+        end
+        describe "custom delimiters" do
+            it "should return 4 for a string of '//;\n1;1;2'" do
+                calc.add("//;\n1;1;2").should == 4
+            end
+        end
+        describe "negative numbers" do
+            it "should fail and report a single negative" do
+                calc.add("1,-1,2").should == "negatives not allowed: -1"
+            end
+            it "should fail and report -2 for a string of '1,1,-2'" do
+                calc.add("1,1,-2").should == "negatives not allowed: -2"
+            end
+            it "should fail and report multiple negatives" do
+                calc.add("1,-1,2,-2,-3").should == "negatives not allowed: -1 -2 -3"
+            end
         end
     end
 end
