c250f429996770d6b010af59a28c3cdf938fe895
Michael (Doc) Norton <docondev@gmail.com>
2011-12-27 17:00:12 -0500

    Clearing tests and code for another run

--- lib/numeral.rb
+++ lib/numeral.rb
@@ -1,24 +1,2 @@
 class Numeral
-    attr_accessor :arabic
-    attr_accessor :roman
-    attr_accessor :subtrahend
-
-    def initialize(*args)
-        if args[0] == args[0].to_i
-            @roman = args[1]
-            @arabic = args[0]
-        else
-            @roman = args[0]
-            @arabic = args[1]
-        end
-    end
-
-    def difference
-        return Numeral.new(@arabic - @subtrahend.arabic, @subtrahend.roman + roman) if @subtrahend
-        nil
-    end
-
-    def ==(compare_to)
-        return @arabic = compare_to.arabic && @roman == compare_to.roman
-    end
 end
--- spec/numeral_spec.rb
+++ spec/numeral_spec.rb
@@ -1,45 +1,4 @@
 require "numeral"
 
 describe "Numeral" do
-    let(:one) {Numeral.new(1, "I")}
-    let(:five) {Numeral.new(5, "V")}
-    let(:ten) {Numeral.new(10, "X")}
-    describe "Basics" do
-        it "should have a roman value and an arabic value" do
-            number = Numeral.new("X", 10)
-            number.arabic.should == 10
-            number.roman.should == "X"
-        end
-        it "should accept values in any order" do
-            number = Numeral.new(5, "V")
-            number.arabic.should == 5
-            number.roman.should == "V"
-        end
-    end
-    describe "subtrahend with" do
-        it "can subtrahend with another numeral" do
-            five.subtrahend = one
-            five.subtrahend.should == one
-        end
-        it "should subtract the subtrahend" do
-            five.subtrahend = one
-            five.difference.should == Numeral.new(4,"IV")
-        end
-        it "should not be aware of any subtrahending rules" do
-            ten.subtrahend = five
-            ten.difference.should == Numeral.new(5,"VX")
-        end
-        it "should be able to see subtrahend numeral details" do
-            ten.subtrahend = one
-            ten.difference.arabic.should == 9
-        end
-    end
-    describe "equality" do
-        it "considers any numeral with same properties to be equal" do
-            five.should == Numeral.new("V", 5)
-        end
-        it "considers any numeral with differing properties to be inequal" do
-            five.should_not == Numeral.new(5, "Y")
-        end
-    end
 end
